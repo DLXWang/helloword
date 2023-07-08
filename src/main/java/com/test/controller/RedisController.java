603b1d7b078bf4f0555c23d2ab3e79b7e1e8cc55
“alexwang” <wx3a07@gmail.com>
2023-07-08 15:21:51 +0800

    <add-event>

--- src/main/java/com/test/controller/RedisController.java
+++ src/main/java/com/test/controller/RedisController.java
@@ -5,7 +5,10 @@ import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 
@@ -20,4 +23,27 @@ public class RedisController {
         redisService.publish();
 
     }
+
+    @GetMapping("/test-redis-set")
+    public void testRedisSet() {
+        redisService.testSet();
+
+    }
+
+    @GetMapping("/test-redis-set-add")
+    public void testRedisSetAdd(@RequestParam String name) {
+        redisService.testAdd(name);
+
+    }
+
+    @GetMapping("/test-redis-set-remove")
+    public void testRedisSetRemove(@RequestParam String name) {
+        redisService.testRemove(name);
+    }
+
+    @GetMapping("/test-redis-set-get")
+    public List<String> testRedisSetGet() {
+        return redisService.testSetGet();
+    }
+
 }
--- src/main/java/com/test/redis/RedisService.java
+++ src/main/java/com/test/redis/RedisService.java
@@ -5,6 +5,7 @@ import org.springframework.data.redis.core.ReactiveStringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
 import java.util.Random;
 
 @RequiredArgsConstructor
@@ -27,6 +28,23 @@ public class RedisService {
 
     public void publish() {
         reactiveRedisTemplate.convertAndSend("testRedisTopic", random.nextInt(10) + "").subscribe();
+    }
+
+    public void testSet() {
+        List<String> values = List.of("BTC", "ETH");
+        reactiveRedisTemplate.opsForSet().add("COIN", values.toArray(new String[0])).subscribe();
+    }
+
+    public void testAdd(String name) {
+        reactiveRedisTemplate.opsForSet().add("COIN", name).subscribe();
+    }
+
+    public void testRemove(String name) {
+        reactiveRedisTemplate.opsForSet().remove("COIN", name).subscribe();
+    }
+
+    public List<String> testSetGet() {
+        return reactiveRedisTemplate.opsForSet().members("COIN").collectList().block();
     }
 
 }
